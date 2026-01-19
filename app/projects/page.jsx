b94deae0eaa7ca6e8e6d63257c729117f44e46ac
Jeremy <bivens.jeremy@gmail.com>
2026-01-19 15:24:21 -0500

    udpate spacing in projects

--- app/projects/page.jsx
+++ app/projects/page.jsx
@@ -14,17 +14,17 @@ const page = () => {
             
             <div className="">
                 {projectData.reverse().map((project, idx) => (
-                    <Link color="foreground" href={project.link} key={idx} isExternal isBlock className="flex flex-col sm:flex-row gap-8 py-6">
-                        <div className="w-full sm:w-1/2">
+                    <Link color="foreground" href={project.link} key={idx} isExternal isBlock className="flex flex-col md:flex-row gap-8 py-6">
+                        <div className="w-full md:w-1/2">
                             <p className="font-bold text-xl pb-4">{project.title}</p>
                             <p className="max-w-md">{project.description}</p>
                         </div>
 
-                        <div className="w-full sm:w-1/2 h-48 relative flex flex-col">
+                        <div className="w-full md:w-1/2 h-48 relative flex flex-col">
                             <Image
                                 src={project.icon}
                                 alt="Project icon"
-                                className="w-3/5 self-end h-full object-cover"
+                                className="w-full lg:w-3/5 self-end h-full object-cover"
                                 removeWrapper
                             />
                         </div>
